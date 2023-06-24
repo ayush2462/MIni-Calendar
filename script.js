de28a4adcd946eed1779ef97b9cc361d2de7e25b
ayush2462 <128534675+ayush2462@users.noreply.github.com>
2023-06-24 13:44:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var date= document.getElementById("date");
+var day= document.getElementById("day");
+var month= document.getElementById("month");
+var year= document.getElementById("year");
+const today = new Date();
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const allMonth = ["January","February","March","April","May","June","July","August","September","October","November","December",]
+date.innerHTML = (today.getDate<10? "0": "") + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonth[today.getMonth()];
+year.innerHTML = today.getFullYear();
